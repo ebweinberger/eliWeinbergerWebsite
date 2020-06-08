a4cebf984000dc28f1215fb9b670e31cba74d060
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-07 21:48:54 -0400

    added responsive sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ProjectComponent } from './project/project.component';
 
 @Component({
@@ -8,4 +8,39 @@ import { ProjectComponent } from './project/project.component';
 })
 export class AppComponent {
   title = 'eliWeinbergerWebsite';
+
+  sidebarStatus: boolean;
+  fade: boolean;
+  mobile: boolean;
+
+  ngOnInit(){
+    this.fade = false;
+
+    if(window.innerWidth > 800){
+      this.sidebarStatus = true;
+    }else{
+      this.sidebarStatus = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      if(window.innerWidth > 800){
+        this.sidebarStatus = true;
+        this.mobile = false;
+      }else{
+        this.sidebarStatus = false;
+        this.mobile = true;
+      }
+    }
+
+  toggleSidebar() {
+    this.sidebarStatus = !this.sidebarStatus;
+    this.fade = !this.fade;
+  }
+
+  sidebarOff(){
+    this.sidebarStatus = false;
+    this.fade = false;
+  }
 }
